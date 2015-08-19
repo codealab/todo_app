8b91314de2623c065b54045dab38eee6ce672ef0
codeamac01 <codeamac01@gmail.com>
2015-08-18 20:50:26 -0500

    Nueva task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+    # render 'tasks/new.html.erb'
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :complete)
+  end
 end
